3f20a3e848824c14f635d0b53f600957c358dd5f
vgauther <vgauther@student.42.fr>
2018-11-21 18:49:38 +0100

    gnl

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: vgauther <vgauther@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 16:20:38 by vgauther          #+#    #+#             */
-/*   Updated: 2018/11/21 16:43:48 by vgauther         ###   ########.fr       */
+/*   Updated: 2018/11/21 18:18:47 by vgauther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include <stdio.h>
-#include  "get_next_line.h"
+#include "../get_next_line.h"
 
 int		main(int argc, char **argv)
 {
@@ -20,7 +20,7 @@ int		main(int argc, char **argv)
 	char	*str;
 
 	fd = open(argv[1], O_RDONLY);
-	while ((get_next_line(fd, &str)) != 0)
+	while ((get_next_line(fd, &str)) == 1)
 	{
 		printf("%s\n", str);
 		free(str);
--- main.sh
+++ main.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-make re -C ./libft
+make re -C ../libft
 echo "libft done"
-gcc main.c $1 -lm -L ./libft -lft
+gcc main.c $1 -lm -L ../libft -lft
 echo "test avec text"
-./a.out text.txt
-
-echo "test avec gnl.c"
-./a.out get_next_line.c
+./a.out text.txt >> solv
+rm solv
+echo "yolo" >> solv
+if [ $(cmp solv safe) = $(cat rez) ]
+then
+	echo "good"
+else
+	echo "diff"
+fi
+echo "............................."
